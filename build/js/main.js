a066f7f66e1931cb24a57e25c71c6660aa61f78c
Aleksei0575 <chudnovets0575@gmail.com>
2023-06-14 20:00:14 +0300

    implements a slider in the gallery block and opening the gallery in a popup

--- build/js/main.js
+++ build/js/main.js
@@ -8,96 +8,94 @@
 // 	});
 // });
 
-// Слайдер для блока offer
-$('.offer-slider').slick({
+// Слайдер для блока gallery
+$('.gallery-slider').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
-  // arrows: false,
-  prevArrow: '<img class="offer-slider__arrows offer-slider__arrows--left" src="img/sprite/icon-arrow-left.svg" alt="Стрелка влево">',
-  nextArrow: '<img class="offer-slider__arrows offer-slider__arrows--right" src="img/sprite/icon-arrow-right.svg" alt="Стрелка вправо">',
-  // variableWidth: true,
+  arrows: true,
+  prevArrow: '<img class="gallery-slider__arrows gallery-slider__arrows--left" src="img/sprite/icon-prew.svg" alt="Стрелка влево">',
+  nextArrow: '<img class="gallery-slider__arrows gallery-slider__arrows--right" src="img/sprite/icon-next.svg" alt="Стрелка вправо">',
+  // // variableWidth: true,
   focusOnSelect: true,
-		responsive: [
-			{
-				breakpoint: 1024,
-				settings: {
-          slidesToShow: 1,
-          centerMode: true,
-          // focusOnSelect: true,
-				}
-			},
-			{
-				breakpoint: 960,
-				settings: {
-					slidesToShow: 1,
-          centerMode: true,
-          arrows: false,
-				}
-			},
-			{
-				breakpoint: 768,
-				settings: {
-					slidesToShow: 1,
-          centerMode: true,
-          arrows: false,
-				}
-			},
-			{
-				breakpoint: 480,
-				settings: {
-					slidesToShow: 1,
-          centerMode: true,
-          arrows: false,
-          variableWidth: true,
-				}
-			},
-		]
+  //slidesPerRow: 2,
+	// 	responsive: [
+	// 		// {
+	// 		// 	breakpoint: 1024,
+	// 		// 	settings: {
+  //     //     slidesToShow: 3,
+  //     //     centerMode: true,
+  //     //     // focusOnSelect: true,
+	// 		// 	}
+	// 		// },
+	// 		// {
+	// 		// 	breakpoint: 960,
+	// 		// 	settings: {
+	// 		// 		slidesToShow: 1,
+  //     //     centerMode: true,
+  //     //     arrows: false,
+	// 		// 	}
+	// 		// },
+	// 		// {
+	// 		// 	breakpoint: 768,
+	// 		// 	settings: {
+	// 		// 		slidesToShow: 1,
+  //     //     centerMode: true,
+  //     //     arrows: false,
+	// 		// 	}
+	// 		// },
+	// 		// {
+	// 		// 	breakpoint: 480,
+	// 		// 	settings: {
+	// 		// 		slidesToShow: 1,
+  //     //     centerMode: true,
+  //     //     arrows: false,
+  //     //     variableWidth: true,
+	// 		// 	}
+	// 		// },
+	// 	]
 });
 
-'use strict';
-// меню
-(function () {
-  var navBtn = document.querySelector('.header__open-menu');
-  var nav = document.querySelector('.header__nav');
+// 'use strict';
+// // меню
+// (function () {
+//   var navBtn = document.querySelector('.header__open-menu');
+//   var nav = document.querySelector('.header__nav');
+//
+//   if (navBtn && nav) {
+//     navBtn.addEventListener('click', openNav);
+//   }
+//
+//   function openNav(evt) {
+//     evt.preventDefault();
+//     evt.stopPropagation();
+//
+//     if (navBtn.classList.contains('header__close-menu')) {
+//       navBtn.classList.remove('header__close-menu');
+//       nav.style.display = '';
+//     } else {
+//       navBtn.classList.add('header__close-menu');
+//       nav.style.display = 'block';
+//     }
+//   }
+// })();
 
-  if (navBtn && nav) {
-    navBtn.addEventListener('click', openNav);
-  }
+$(document).ready(function() {
+  $('.gallery-slider').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    tLoading: 'Loading image #%curr%...',
+    mainClass: 'mfp-img-mobile',
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+    },
+    // image: {
+    //   tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+    //   titleSrc: function(item) {
+    //     return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+    //   }
+    // }
+  });
+});
 
-  function openNav(evt) {
-    evt.preventDefault();
-    evt.stopPropagation();
-
-    if (navBtn.classList.contains('header__close-menu')) {
-      navBtn.classList.remove('header__close-menu');
-      nav.style.display = '';
-    } else {
-      navBtn.classList.add('header__close-menu');
-      nav.style.display = 'block';
-    }
-  }
-})();
-
-'use strict';
-// меню
-(function () {
-  var navBtn = document.querySelector('.header__open-menu');
-  var nav = document.querySelector('.header__nav');
-
-  if (navBtn && nav) {
-    navBtn.addEventListener('click', openNav);
-  }
-
-  function openNav(evt) {
-    evt.preventDefault();
-    evt.stopPropagation();
-
-    if (navBtn.classList.contains('header__close-menu')) {
-      navBtn.classList.remove('header__close-menu');
-      nav.style.display = '';
-    } else {
-      navBtn.classList.add('header__close-menu');
-      nav.style.display = 'block';
-    }
-  }
-})();
